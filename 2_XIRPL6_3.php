ab377b60ba90c0020075c16cc943b99b1018997f
zaenarifputra <zaenarifputraainurdin@gmail.com>
2021-10-05 19:31:24 +0700

    menambahkan spasi

--- 2_XIRPL6_3.php
+++ 2_XIRPL6_3.php
@@ -3,14 +3,18 @@
 <body>
 
 <?php  
+echo ("Perulangan");
+echo "</br>";
+echo "</br>";
 date_default_timezone_set("Asia/Jakarta"); 
-echo "<br>";
 echo date("d-m-Y H:i");
+echo "</br>";
+echo "</br>";
 for($i=0;$i<=5;$i++){  
 for($j=5-$i;$j>=1;$j--){  
 echo "* ";  
 }  
-echo "<br>";  
+echo "</br>";  
 }  
 ?>  
 </body>
