1683f0ee062bc86c43f79da990e1364c94dca938
zaenarifputra <91659924+zaenarifputra@users.noreply.github.com>
2021-10-09 14:13:48 +0700

    Add files via upload

---
+++ 3_XIRPL6_3.php
@@ -0,0 +1,18 @@
+
+<?Php
+$data[0]['nama'] = "Satria";
+$data[0]['kelas'] = "RPL 1";
+$data[1]['nama'] = "Arsyi";
+$data[1]['kelas'] = "RPL 2";
+$data[2]['nama'] = "Dhika";
+$data[2]['kelas'] = "RPL 3";
+$data[3]['nama'] = "Evan";
+$data[3]['kelas'] = "RPL 4";
+$data[4]['nama'] = "Fabian";
+$data[4]['kelas'] = "RPL 5";
+$data[5]['nama'] = "Irfan";
+$data[5]['kelas'] = "RPL 6";
+echo "<pre>";
+print_r($data);
+echo "<pre>";
+?>
